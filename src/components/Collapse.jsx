c8ce7337a6ef842c9538b050ca6ff8d9f59172e7
qachar hosna <qachar.hosna@gmail.com>
2024-09-28 21:40:47 +0200

    ajout collaps dans about

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,34 @@
+
+import { useState } from "react";
+import svgFleche from "../assets/image/arrow.png";
+import PropTypes from "prop-types";
+
+const Collapse = ({ title, children }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const handleToggle = () => {
+      setIsVisible(!isVisible);
+    };
+  
+    return (
+      <div className={`collapse ${isVisible ? "visible" : ""}`}>
+        <div className="headCollapse" onClick={handleToggle}>
+          <h3>{title}</h3>
+          <img
+            src={svgFleche}
+            alt="flèche"
+            className={`arrow ${isVisible ? "down" : ""}`}
+          />
+        </div>
+        <div className={`paragraph ${isVisible ? "animate" : ""}`}>
+          {children}
+        </div>
+      </div>
+    );
+  };
+  
+  Collapse.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired,
+  };
+  
+  export default Collapse;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,18 @@
 import Bannerapropos from "../components/Bannerapropos";
+import Collapse from "../components/Collapse";
+import aproposData from "../data/apropos.json";
+
 
 
 const About = () => {
     return (
         <div className="about">
             <Bannerapropos />
+            {aproposData.map((item, index) => (
+                <Collapse key={index} title={item.title}>
+                    {item.text}
+                </Collapse>
+            ))}
         </div>
         
     );
